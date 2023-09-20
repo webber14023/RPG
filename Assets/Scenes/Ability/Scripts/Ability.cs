de7efd73da321202e3a66c64da787149900cbb26
webber14023 <73771426+webber14023@users.noreply.github.com>
2023-09-20 19:21:00 +0800

    Add skill
    新增攻擊方式

---
+++ Assets/Scenes/Ability/Scripts/Ability.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Ability : ScriptableObject
+{
+    public new string name;
+    public float cooldownTime;
+    public float activeTime;
+
+    public virtual void Activate(GameObject parent) {}
+    public virtual void BeginCooldown(GameObject parent) {}
+}
